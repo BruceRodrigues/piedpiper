4075e98e8835625d96c3b595f5611ec8ee5f6cb4
Bruce Rodrigues <brucerodrigues6@gmail.com>
2019-11-20 22:27:37 -0300

    fake login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+
+import LoginView from './login/LoginView'
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Route path='/login' component={LoginView} />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
