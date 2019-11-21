b3dfbb2f5577cb5d330fd3fa86d482daae53cbe4
Bruce Rodrigues <brucerodrigues6@gmail.com>
2019-11-20 22:13:51 -0300

    app packaging jar

--- backend/src/main/java/br/piedpiper/backend/BackendApplication.java
+++ backend/src/main/java/br/piedpiper/backend/BackendApplication.java
@@ -1,13 +1,10 @@
 package br.piedpiper.backend;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class BackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
 
 }
--- backend/src/test/java/br/piedpiper/backend/BackendApplicationTests.java
+++ backend/src/test/java/br/piedpiper/backend/BackendApplicationTests.java
@@ -1,9 +1,12 @@
 package br.piedpiper.backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = { BackendApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendApplicationTests {
 
 	@Test
